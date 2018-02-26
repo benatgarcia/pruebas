4fcc88a8faa56d633f58d2c26a79a01a835380fc
benatgarcia <benatgu37@gmail.com>
2018-02-26 14:00:45 +0100

    clase hola añadida

---
+++ hola.java
@@ -0,0 +1,28 @@
+
+/**
+ *  class hola  
+ * 
+ * @author  
+ * @version  
+ */
+public class hola
+{
+     
+    private int x;
+
+    /**
+     * Constructor de la clase hola
+     */
+    public hola()
+    {
+     
+    }
+
+    /**
+     *  
+     */
+    public void metodo()
+    {
+         
+    }
+}
